2e1d1d9e31124994f30d8409e23ccf6fc8a70baa
Mukund Kumar <banananarick69@gmail.com>
2022-10-31 16:05:14 +0530

    linked urls and added templates

--- webpage/webpage/urls.py
+++ webpage/webpage/urls.py
@@ -13,12 +13,14 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
+from importlib.resources import path
 from django.contrib import admin
 from django.urls import path
 
 from boards import views
 
 urlpatterns = [
+    path("", views.home, name='home'),
+    path("boards/<int:pk>/", views.board_topics, name="board_topics"),
     path("admin/", admin.site.urls),
-    path("",views.home,name="home"),
 ]
